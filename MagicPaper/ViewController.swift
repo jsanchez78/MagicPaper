6083d7da9e4d32d3a34c328ecba52b54c7cf6d68
jsanchez78 <jsanch75@uic.edu>
2020-05-22 10:39:56 -0500

    AR Tracking complete

--- MagicPaper/ViewController.swift
+++ MagicPaper/ViewController.swift
@@ -22,19 +22,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        //Make Newspaper Image recognizable
+        let configuration = ARImageTrackingConfiguration()
+        
+        //Track images within this bundle
+        if let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "NewspaperImages", bundle: Bundle.main){
+            
+            configuration.trackingImages = trackedImages
+            
+            configuration.maximumNumberOfTrackedImages = 1
+            
+        }
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -46,30 +49,37 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
+        // Create plane for newspaper
+        
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
+        
+        if let imageAnchor = anchor as? ARImageAnchor{
+            
+            let videoNode = SKVideoNode(fileNamed: "Newspaper.mp4")
+            
+            videoNode.play()
+            
+            // Display Spritekit element via SceneKit
+            
+            let videoScene = SKScene(size: CGSize(width: 240, height: 180)) //Height varies by resolution of video
+            
+            videoNode.position = CGPoint(x: videoNode.size.width / 2, y: videoNode.size.height / 2)
+            
+            videoNode.yScale = -1.0
+            
+            videoScene.addChild(videoNode)
+            
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            
+            plane.firstMaterial?.diffuse.contents = videoScene
+            
+            let planeNode = SCNNode(geometry: plane)
+            
+            planeNode.eulerAngles.x = -.pi/2    //Gif will be parallel to newspaper
+            
+            node.addChildNode(planeNode)
+        }
         return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
